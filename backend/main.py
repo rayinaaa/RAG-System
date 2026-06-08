52ffb4d0355922e612e7c97072efc90e59e249b3
rayinaaa <rayina.fathi@gmail.com>
2026-06-08 21:49:45 +0530

    cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,22 @@ from backend.routes import chat, documents, health, upload
 from backend.services.config import settings
 from backend.services.logging_config import configure_logging
 from backend.services.storage import storage
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://rag-system-sand-phi.vercel.app/"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
